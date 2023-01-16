0b8332feab617d9a4a41b0eb4fc2dc2b87a3f333
Zeljko Dzakic <zdzakic@gmail.com>
2023-01-16 18:05:38 +0100

    laptop push

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -10,6 +10,7 @@ import plotly.express as px
 spacex_df = pd.read_csv("spacex_launch_dash.csv")
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
+lounch_sites = list(spacex_df['Launch Site'].unique())
 
 # Create a dash application
 app = dash.Dash(__name__)
@@ -21,6 +22,24 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                 # TASK 1: Add a dropdown list to enable Launch Site selection
                                 # The default select value is for ALL sites
                                 # dcc.Dropdown(id='site-dropdown',...)
+                                dcc.Dropdown(id='site-dropdown',
+                                             options=[
+                                                {'label': 'All Sites',
+                                                 'value': 'ALL'},
+                                                {'label': lounch_sites[0],
+                                                 'value': lounch_sites[0]},
+                                                {'label': lounch_sites[1],
+                                                 'value': lounch_sites[1]},
+                                                {'label': lounch_sites[2],
+                                                 'value': lounch_sites[2]},
+                                                {'label': lounch_sites[3],
+                                                 'value': lounch_sites[3]},
+                                             ],
+                                             value='ALL',
+                                             placeholder="Select a Launch Site here",
+                                             searchable=True
+                                             ),
+
                                 html.Br(),
 
                                 # TASK 2: Add a pie chart to show the total successful launches count for all sites
@@ -40,10 +59,21 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
 # TASK 2:
 # Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
 
-# TASK 4:
-# Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
 
+@app.callback(Output(component_id='success-pie-chart', component_property='figure'),
+              Input(component_id='site-dropdown', component_property='value'))
+def get_pie_chart(entered_site):
+    filtered_df = spacex_df
+    if entered_site == 'ALL':
+        fig = px.pie(filtered_df, values='class',
+                     names='pie chart names',
+                     title='title')
+        return fig
+    else:
+        # return the outcomes piechart for a selected site
+        # TASK 4:
+        # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
 
-# Run the app
+        # Run the app
 if __name__ == '__main__':
     app.run_server()
